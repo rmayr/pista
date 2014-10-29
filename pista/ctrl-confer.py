0c8634e408e03ad00a37516f95c13eddd6829ab1
Jan-Piet Mens <jpmens@gmail.com>
2014-10-29 23:09:22 +0100

    Logging, trackList from db, check requested track matches subscription in db

--- pista/ctrl-confer.py
+++ pista/ctrl-confer.py
@@ -7,14 +7,29 @@ __copyright__ = 'Copyright 2014 Jan-Piet Mens'
 import sys
 sys.path.insert(0, './lib')
 sys.path.insert(0, './../o2s/lib')
+import os
+import logging
 import bottle
 from bottle import response, template, static_file, request
 import json
 import time
 from cf import conf
-from authschema import User, fn, sql_db
+from authschema import User, Acl, fn, sql_db
 from dbschema import Location
 import hashing_passwords as hp
+import paho.mqtt.client as paho
+
+
+SCRIPTNAME = os.path.splitext(os.path.basename(__file__))[0]
+LOGFILE    = os.getenv(SCRIPTNAME.upper() + 'LOG', SCRIPTNAME + '.log')
+LOGLEVEL   = logging.INFO
+#LOGLEVEL   = logging.DEBUG
+LOGFORMAT  = '%(asctime)-15s %(levelname)-5s [%(module)s] %(message)s'
+
+logging.basicConfig(filename=LOGFILE, level=LOGLEVEL, format=LOGFORMAT)
+logging.info("Starting %s" % SCRIPTNAME)
+logging.info("INFO MODE")
+logging.debug("DEBUG MODE")
 
 app = application = bottle.Bottle()
 
@@ -22,7 +37,6 @@ def notauth(reason):
     return bottle.HTTPResponse(status=403, body=json.dumps({"message": reason}))
 
 def auth(username, password, apns_token=None):
-    print "***DEBUG AUTH CONF: username=%s, pw=[%s], TOK=%s" % ( username, password, apns_token)
 
     if username is None or password is None:
         return False
@@ -30,7 +44,7 @@ def auth(username, password, apns_token=None):
     try:
         sql_db.connect()
     except Exception, e:
-        print str(e)
+        logging.error("%s" % str(e))
         return False
 
     pwhash = None
@@ -38,16 +52,15 @@ def auth(username, password, apns_token=None):
         u = User.get(User.username == username)
         pwhash = u.pwhash
     except User.DoesNotExist:
-        print "User ", username, " does not exist"
+        logging.debug("User ", username, " does not exist")
         return False
     except Exception, e:
         raise
 
-    print "GOT hash == [%s]" % pwhash
 
 
     match = hp.check_hash(password, pwhash)
-    print "Hash match for [%s]: %s" % (username, match)
+    logging.debug('Hash match for %s (%s): %s' % (username, pwhash, match))
 
     if match == True and apns_token is not None:
         tstamp = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
@@ -62,7 +75,6 @@ def show_index():
 
 @app.route('/ext/conf', method='POST')
 def conf():
-    status = 200
     resp = {}
     data = bottle.request.body.read()
 
@@ -71,16 +83,28 @@ def conf():
     apns_token = request.forms.get('token')
 
     authorized = auth(username, password, apns_token)
+
+    logging.info("CONF: (username=%s, token=%s) authorized=%s" % (username, apns_token, authorized))
+
     if authorized == False:
         return notauth("Not authenticated")
 
-    if not authorized:
-        status = 403
-    else:
-        topics = [ 'owntracks/+/MV', 'owntracks/+/+' ]  # FIXME: from DB
-        resp = {
+    topic_list = []
+
+    # Select list of topics for this user. Attempt to expand %u which the client
+    # won't understand. %c loses out -- that won't work, so no point returning
+    # those.
+    try:
+        for sub in Acl.select().where(Acl.username == username):
+            new_sub = sub.topic.replace('%u', username)
+            if '%c' not in new_sub:
+                topic_list.append(new_sub)
+    except Exception, e:
+        logging.error("Can't query ACL for topic list: %s" % (str(e)))
+
+    resp = {
             '_type'         : 'configuration',
-            'topicList'     : topics,
+            'topicList'     : topic_list,
             'host'          : 'demo.owntracks.de',
             'port'          : 8883,
             'tls'           : 1,
@@ -96,13 +120,15 @@ def conf():
         resp['trackurl'] = None
 
     response.content_type = 'application/json'
-    response.status = status
+    response.status = 200
     # return json.dumps(resp, sort_keys=True, separators=(',',':'))
     return json.dumps(resp, sort_keys=True, indent=2)
 
 
 @app.route('/ext/ctrl/cacert.pem', method='GET')
 def cacert():
+
+    # FIXME
     f = open('demo-8883.crt')
     pem = f.read()
     f.close()
@@ -123,48 +149,82 @@ def ctrl_trackdump(user):
     username = request.forms.get('username')
     password = request.forms.get('password')
     tid = request.forms.get('tid')
-    nrecs = request.forms.get('nrecs')
+    try:
+        nrecs = int(request.forms.get('nrecs'))
+    except:
+        nrecs = 10
     topic = request.forms.get('topic')
 
     authorized = auth(username, password)
+
+    logging.info("TRACK: (username=%s) authorized=%s" % (username, authorized))
+
     if authorized == False:
         return notauth("Not authenticated")
 
     if topic is None:
         return notauth("topic is empty")
 
+
+    # Check whether this user is authorized to download track for this topic.
+    # Get list of topics from ACL table and compare. Caveat: %c escapes are
+    # not supported here and will thus not match.
+
+    track_authorized = False
+    message = "Not authorized"
+
+    try:
+        query = (Acl.select(Acl). where(
+                    (Acl.username == username)
+                    ))
+        for s in query.naive():
+            sub    = s.topic
+
+            new_sub = sub.replace('%u', username)
+            matches = paho.topic_matches_sub(sub, topic)
+            logging.debug("sub %s (%s) => %s" % (sub, new_sub, matches))
+            if matches:
+                track_authorized = True
+                break
+    except Exception, e:
+        logging.error("Can't query ACL: %s" % (str(e)))
+
+
+
     if nrecs is None or int(nrecs) < 1:
         nrecs = 50
-
-    print "user=[%s:%s] TID=[%s] nrecs=%s TOPIC=[%s]" % (username, password, tid, nrecs, topic)
+    if nrecs > 600:
+        nrecs = 600
 
     track = []
 
-    query = (Location
-                .select(Location).
-                where(
-                    (Location.topic == topic)
-                    )
-                ).order_by(Location.tst.desc()).limit(nrecs)
-    for l in query.naive():
+    if track_authorized == True:
+        message = "OK"
+        query = (Location
+                    .select(Location).
+                    where(
+                        (Location.topic == topic)
+                        )
+                    ).order_by(Location.tst.desc()).limit(nrecs)
+        for l in query.naive():
 
-        dbid    = l.id
-        lat     = float(l.lat)
-        lon     = float(l.lon)
-        dt      = l.tst
+            dbid    = l.id
+            lat     = float(l.lat)
+            lon     = float(l.lon)
+            dt      = l.tst
 
-        try:
-            tp = {
-                'lat' : float(l.lat),
-                'lon' : float(l.lon),
-                'tst' : int(dt.strftime('%s')),
-            }
+            try:
+                tp = {
+                    'lat' : float(l.lat),
+                    'lon' : float(l.lon),
+                    'tst' : int(dt.strftime('%s')),
+                }
 
-            track.append(tp)
-        except:
-            pass
+                track.append(tp)
+            except:
+                pass
 
-    message = "OK"  # or clear-text error shown to user
+    logging.info("TRACK: (username=%s, tid=%s, topic=%s) RETURN %s recs" % (username, tid, topic, len(track)))
 
     data = {
         'topic'    : topic,
