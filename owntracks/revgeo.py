eb0512af7e01f2b4f3cb6266516dfb1a163d4c02
Jan-Piet Mens <jpmens@gmail.com>
2014-11-05 18:48:00 +0100

    move to modules

---
+++ owntracks/revgeo.py
@@ -0,0 +1,105 @@
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import urllib2
+import geohash # https://code.google.com/p/python-geohash/
+from owntracks.dbschema import db, Geo
+
+class RevGeo(object):
+    def __init__(self, conf, storage=True, host='localhost', port=8081):
+        self.enabled = conf.get('enabled', False)
+        self.host = host
+        self.port = port
+        self.hashlen = conf.get('ghashlen', 5)
+        self.storage = storage
+        self.region_bias = conf.get('region_bias', 'de')
+
+    def rev(self, lat, lon, api='geonames'):
+        if not self.enabled:
+            return None
+
+        lat = float(lat)
+        lon = float(lon)
+
+
+        ''' Look up the `ghash' geohash; if found, return the cached
+            content, else perform lookup and store in cache. '''
+
+        ghash = geohash.encode(lat, lon)[:self.hashlen]
+        if self.storage:
+            try:
+                g = Geo.get(Geo.ghash == ghash)
+                return dict(ghash=ghash, cc=g.cc, addr=g.addr, cached=1)
+            except Geo.DoesNotExist:
+                pass
+            except Exception, e:
+                raise
+
+        methods = {
+            'geonames' : (1, self._geoname),
+            'google'   : (2, self._google),
+        }
+
+        if api not in methods:
+            return None
+
+
+        src = methods[api][0]
+        func = methods[api][1]
+        data = func(lat, lon)
+        if data is None or type(data) is not dict:
+            return None
+
+        ''' Store data in cache '''
+
+        data['src'] = src
+        data['ghash'] = ghash
+        if self.storage:
+            try:
+                g = Geo(**data)
+                g.save()
+            except Exception, e:
+                logging.warn("Cannot store GEO in DB: %s" % (str(e)))
+
+        return dict(ghash=ghash, cc=data['cc'], addr=data['addr'], cached=0)
+
+    def _geoname(self, lat, lon):
+        try:
+            r = urllib2.urlopen('http://%s:%s/%s,%s' % (self.host, self.port, lat, lon))
+            s = r.read().decode('utf-8') 
+            
+            (geoname, km, cc, addr) = s.split('|')
+            revgeo = {
+                'geoname' : geoname,
+                'km' : km,
+                'cc' : cc,
+                'addr' : addr,
+            }
+            
+            return revgeo
+        except Exception, e:
+            raise
+            print "\t%s" % str(e)
+
+    def _google(self, lat, lon):
+
+        try:
+            # https://developers.google.com/maps/documentation/geocoding/
+            maplang = self.region_bias
+            url = 'http://maps.googleapis.com/maps/api/geocode/json' + \
+                    '?latlng=%s,%s&sensor=false&language=%s&region=%s' % (lat, lon, maplang, self.region_bias)
+            google_data = json.load(urllib2.urlopen(url))
+
+            revgeo = { }
+            if 'status' in google_data and google_data['status'] != 'OK':
+                # FIXME: log status
+                return None
+
+            revgeo['addr'] = google_data['results'][0]['formatted_address']
+            for el in google_data['results'][0]['address_components']:
+                if 'country' in el['types']:
+                    revgeo['cc'] =  el['short_name']
+            return revgeo
+        except:
+            return None
---
+++ owntracks/waypoints.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
+__copyright__ = 'Copyright 2014 Jan-Piet Mens'
+
+import time
+import datetime
+import json
+import os
+from owntracks.dbschema import db, Waypoint
+from geolocation import GeoLocation
+import persist
+import logging
+import hashlib
+
+TRIGGER_ENTER   = 1
+TRIGGER_LEAVE   = 0
+
+
+class WP(object):
+    def __init__(self, persistence_db, mosq, maptopic=None, alert_topic=None, alert_keys=None, watcher_topic=None):
+        self.logging = logging.getLogger(__name__)
+        self.persistence_db = persistence_db
+        self.mosq = mosq
+        self.maptopic = maptopic
+        self.watcher_topic = watcher_topic
+        self.alert_topic = alert_topic
+        self.alert_keys = []    # list of keys to add to JSON payload on fence alerts
+
+        if alert_keys is not None:
+            for elem in alert_keys.split():
+                self.alert_keys.append(elem)
+
+        self.tlist = persist.PersistentDict(self.persistence_db, 'c', format='pickle')
+        self.wplist = self.load_waypoints()
+
+        self.logging.debug("alert_keys: {0}".format(self.alert_keys))
+
+    def load_waypoints(self):
+
+        wplist = []
+        for w in Waypoint.select():
+            try:
+                lat     = float(w.lat)
+                lon     = float(w.lon)
+                meters  = int(w.rad)
+                desc    = w.waypoint
+                wptopic   = w.topic.replace('/waypoints', '')
+
+                if meters == 0:
+                    continue
+
+                onepoint = WayPoint(lat, lon, meters, desc)
+                wplist.append(onepoint)
+                self.logging.debug("Loading {0}".format(onepoint))
+
+                if self.maptopic:
+                    fence_data = {
+                        '_type'    : 'fence',
+                        'lat'      : lat,
+                        'lon'      : lon,
+                        'radius'   : meters,
+                        'waypoint' : desc,
+                    }
+                    # I need a "key" to publish to maptopic so that one waypoint
+                    # doesn't clobber the previous if both were originally to 
+                    # same topic.
+
+                    unique = "{0}-{1}-{2}".format(wptopic, lat, lon)
+                    hash_object = hashlib.sha1(unique)
+                    unique_sha = hash_object.hexdigest()
+
+                    try:
+                        # fence_topic = self.maptopic + "/" + unique_sha
+                        fence_topic = self.maptopic.format(wptopic) + "/" + unique_sha
+
+                        self.mosq.publish(fence_topic, json.dumps(fence_data), qos=0, retain=True)
+                    except Exception, e:
+                        self.logging.warn("Cannot publish fence: {0}".format(str(e)))
+            except:
+                pass
+
+        return wplist
+
+    def alert(self, wp, trigger, item, meters, km):
+
+        if self.alert_topic is None:
+            return
+
+        payload = {
+                '_type'     : 'alert',
+                'trigger'   : trigger,
+                'meters'    : int(meters),
+                'km'        : km,
+                'wplat'     : wp.lat,
+                'wplon'     : wp.lon,
+                'wpname'    : wp.description,
+            }
+
+        if trigger == TRIGGER_ENTER:
+            payload['event'] = 'enters'
+        else:
+            payload['event'] = 'leaves'
+
+        key_list = self.alert_keys
+        if key_list is None or len(key_list) == 0:
+            key_list = list(item.keys())
+        for key in key_list:
+            if key in item:
+                if key == '_type':
+                    continue
+                if key == 'topic':
+                    payload['wptopic'] = item[key]
+                else:
+                    payload[key] = item[key]
+
+
+        try:
+            self.mosq.publish(self.alert_topic, json.dumps(payload, sort_keys=True), qos=0, retain=False)
+        except Exception, e:
+            self.logging.warn("Cannot publish fence: {0}".format(str(e)))
+
+        if self.watcher_topic is not None:
+            message = "ALERT: {tid}: {event} {wpname} at {tstamp}. Distance: {meters}m".format(**payload)
+            self.logging.info(message)
+            print message
+            try:
+                self.mosq.publish(self.watcher_topic, message, qos=0, retain=False)
+            except Exception, e:
+                self.logging.warn("Cannot publish fence: {0}".format(str(e)))
+
+
+    def check(self, item):
+
+        lat = item['lat']
+        lon = item['lon']
+        tid = item['tid']
+        topic = item['topic']
+
+        try:
+            here = GeoLocation.from_degrees(lat, lon)
+            for wp in self.wplist:
+                trigger = None
+                meters = 0
+                try:
+                    meters = here.distance_to(wp.point) * 1000.
+                except:
+                    pass
+
+                km = "{:.2f}".format( float(meters / 1000.0) )
+
+                movement = {
+                    'km' : km,
+                    'tid' : tid,
+                    'topic' : topic,
+                    'event' : None,
+                }
+                very_near = meters <= wp.meters
+                if very_near is False:
+                    if topic in self.tlist:
+                        if self.tlist[topic] == wp:
+                            trigger = TRIGGER_LEAVE
+                            self.alert(wp, trigger, item, meters, km)
+
+                            del self.tlist[topic]
+                            self.tlist.sync()
+
+                if very_near is True:
+                    if topic not in self.tlist:
+                        trigger = TRIGGER_ENTER
+                        self.tlist[topic] = wp
+                        self.tlist.sync()
+
+                        self.alert(wp, trigger, item, meters, km)
+                    else:
+                        # Optional:
+                        # msg = "%s STILL AT  %s (%s km)" % (topic, self.tlist[topic], km)
+                        # print msg
+                        return
+        except:
+            raise
+            pass
+
+class WayPoint(object):
+    def __init__(self, lat, lon, meters=1000, description=None):
+        self.lat = lat
+        self.lon = lon
+        self.meters = meters
+        self.description = description
+
+        self.point = GeoLocation.from_degrees(lat, lon)
+        self.state = None
+
+    def point(self):
+        return self.point
+
+    def __str__(self):
+        return ("{0:s} ({1:.4f}, {2:.4f})").format(
+                    self.description.encode('utf-8'), self.lat, self.lon)
+
+    def __eq__(self, other):
+        eq = False
+
+        if self.lat == other.lat and \
+            self.lon == other.lon:
+            eq = True
+        return eq
+
