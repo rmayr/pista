fac1053ef5d928cb6e13a58efe52a46e16aa614c
Jan-Piet Mens <jpmens@gmail.com>
2014-11-05 18:46:34 +0100

    centralize config

--- owntracks/__init__.py
+++ owntracks/__init__.py
@@ -2,3 +2,8 @@
 
 __author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
 __copyright__ = 'Copyright 2014 Jan-Piet Mens'
+
+import os
+import config
+
+cf = config.Config(os.getenv('O2SCONFIG', 'o2s.conf'))
