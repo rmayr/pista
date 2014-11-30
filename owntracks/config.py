a0ab89b7015ee6d7846ffb5407222fbf8374aa94
Jan-Piet Mens <jpmens@gmail.com>
2014-11-30 17:01:14 +0100

    license/author

--- owntracks/config.py
+++ owntracks/config.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
+__copyright__ = 'Copyright 2014 Jan-Piet Mens'
+__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+
 import os
 from ConfigParser import RawConfigParser, NoOptionError
 import codecs
