1edb1c2d193843fa554009c8d63c1c1b5d748825
Jan-Piet Mens <jpmens@gmail.com>
2014-12-20 12:37:15 +0100

    add license

--- _demo/simulator.py
+++ _demo/simulator.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
+__copyright__ = 'Copyright 2014 Jan-Piet Mens'
+__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
 
 from data import *
 from data import _dM, _dK, _dB, _dH, _dN, _dG
